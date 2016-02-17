ce556aab30f856b0446c7c37117a43b5eafab31b
andreasoikonomou <andreas.oikonomou@ntu.ac.uk>
2016-02-17 18:56:54 +0000

    Basic HTML 5 scaffolding with jQuery (plus css and js)

---
+++ main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by andreasoikonomou on 17/02/2016.
+ */
+
+
+function myArray(){
+
+var myArray = new Array();
+myArray = ["a", "b", "c"];
+alert("Blah " + myArray);
+
+}
+
+function jQueryHelloWorld(){
+
+    // Some jQuery stuff
+
+
+}
