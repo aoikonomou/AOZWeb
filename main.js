de3bc7828c69518e5b3e50d9786c59568ae9c777
andreasoikonomou <andreas.oikonomou@ntu.ac.uk>
2016-02-17 23:09:29 +0000

    Creating basic game interface. Need to clarify div positioning

--- main.js
+++ main.js
@@ -3,28 +3,30 @@
  */
 
 
-function jQueryHelloWorld(){
+function jQueryHelloWorld() {
 
     // Some javascript/jQuery stuff
     //Print something in the document with jQuery
     console.log("Writing to console");
-    alert("Writing to popup window ");
+    //alert("Writing to popup window ");
     //Write to HTML
-    $( "#div1" ).append( "<p>jQuery wrote here</p>" );
+    $("#div1").append("<p>jQuery wrote here</p>");
 
 
 }
 
-function myArray(){
+function myArray() {
 
-    var myArray = [[1,50,100],[1,50,100],[1,50,100]];
+    var myArray = [[1, 50, 100], [1, 50, 100], [1, 50, 100]];
     console.log("Blah " + myArray);
 
-    for(i=0;i<3;i++){
+    for (i = 0; i < 3; i++) {
 
-        for(j=0;j<3;j++){
+        console.log(i);
 
-console.log(myArray[i][j]);
+        for (j = 0; j < 3; j++) {
+
+            console.log(myArray[i][j]);
 
         }
 
@@ -32,25 +34,21 @@ console.log(myArray[i][j]);
 
 }
 
-function drawArray(){
-    for (i=1;i<10;i++){
+function drawArray() {
+    for (i = 0; i < 3; i++) {
 
-        $("#body").append("<div>Horizontal</div>").offset({top: 1, left:(i*10) })
+        for (j = 0; j < 3; j++) {
 
-
-        for (j=1;j<10;j++){
-
-            $("#body").append("<div>Vertical</div>").offset({top: (i*10), left:1})
+            $("#body").append("<div>Vertical</div>").offset({top: (i * 10), left: 1})
 
 
         }
     }
 
 }
-
 
 
 // jQuery drag and drop
-$(function() {
-    $( "#blah" ).draggable();
+$(function () {
+    $("div").draggable();
 });
