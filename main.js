97d5cbbe0abed0e735b2fbac1847c7109acdd573
andreasoikonomou <andreas.oikonomou@ntu.ac.uk>
2016-03-07 22:45:19 +0000

    Cleaning up code and trying AngularJS

--- main.js
+++ main.js
@@ -12,48 +12,28 @@ function jQueryHelloWorld() {
     //Write to HTML
     $("#ui").append("<p>jQuery wrote here</p>");
 
-
 }
 
-function myArray() {
-
-    var myArray = [[1, 50, 100], [1, 50, 100], [1, 50, 100]];
-    console.log("Blah " + myArray);
-
-    for (i = 0; i < 3; i++) {
-
-        console.log(i);
-
-        for (j = 0; j < 3; j++) {
-
-            console.log(myArray[i][j]);
-
-        }
-
-    }
-
-}
 
 function drawArray() {
 
-    x=9;
-    y=9;
-    xOffset = 25;
-    yOffset = 100;
+    boardWidth=9;
+    boardHeight=9;
+    xTileOffset = 25;
+    yTileOffset = 100;
 
     //Temporary document location offset until I clarify if I can position relatively with css
 
-    for (i = 0; i < x; i++) {
+    for (i = 0; i < boardWidth; i++) {
 
-        for (j = 0; j < y; j++) {
+        for (j = 0; j < boardHeight; j++) {
 
             $("#body").append("<div id='" + i +j+ "'class='tile'>" +i +j+ "</div>");
-            $("#"+i+j).css({left: ((i * 65)+xOffset), top: ((j * 65)+yOffset)});
+            $("#"+i+j).css({left: ((i * 65)+xTileOffset), top: ((j * 65)+yTileOffset)});
             console.log(i, j);
 
         }
     }
-
 }
 
 
